782142ba53668ea94343b3be792041829eabfbff
mahesh-sanikommu <sanikommumahesh@gmail.com>
2023-08-21 22:56:49 -0700

    Update integration.test.js

--- integration.test.js
+++ integration.test.js
@@ -1,18 +1,12 @@
 const { fetchData } = require('../../src/fetch-data');
 const request = require('supertest');
-const { app, server, fetchDataAndUpdate } = require('../../src/server');
+const { app, server, fetchDataAndUpdate, intervalId } = require('../../src/server');
 
 jest.mock('../../src/fetch-data');
 
 describe('/metrics endpoint', () => {
-    let dataFetchInterval;
-    let fetchInterval = 1000;
-    beforeAll(() => {
-        dataFetchInterval = setInterval(fetchDataAndUpdate, fetchInterval);
-    });
-
     afterAll((done) => {
-        clearInterval(dataFetchInterval);
+        clearInterval(intervalId);
         server.close(done);
     });
 
