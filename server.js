bd579784bcaf55f73a91e95b68a2953b65c4c391
mahesh-sanikommu <sanikommumahesh@gmail.com>
2023-08-21 18:13:24 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require('express');
-const dataManager = require('./data-manager');
+const fetchData = require('./fetch-data');
+
+let data = {};
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+
+const fetchDataAndUpdate = async () => {
+    try {
+        data = await fetchData();
+        console.log('Data fetched:', data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+  
+  // Fetch data immediately on module import
+fetchDataAndUpdate();
+  
+// Fetch data every 10 seconds
+const fetchInterval = 10000; // 10 seconds
+setInterval(fetchDataAndUpdate, fetchInterval);
 
 app.get('/metrics', (req, res) => {
-  res.json(dataManager.getData());
+  res.json(data);
 });
 
 const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
   
-module.exports = { app, server }; 
+module.exports = { app, server, fetchDataAndUpdate }; 
