1b133f08952e8f99306be1b68b7c861708f4653c
mahesh-sanikommu <sanikommumahesh@gmail.com>
2023-08-21 20:19:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,43 @@
 const express = require('express');
-const fetchData = require('./fetch-data');
-
-let data = {};
+const { fetchData } = require('./fetch-data')
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
+
+let data = null;
+
+app.use(express.json());
 
 const fetchDataAndUpdate = async () => {
-    try {
-        data = await fetchData();
-        console.log('Data fetched:', data);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
+  try {
+      data = await fetchData();
+  } catch (error) {
+      console.error('Error fetching data:', error);
+  }
 };
-  
-  // Fetch data immediately on module import
 fetchDataAndUpdate();
-  
-// Fetch data every 10 seconds
-const fetchInterval = 10000; // 10 seconds
+const fetchInterval = 10000;
 setInterval(fetchDataAndUpdate, fetchInterval);
 
-app.get('/metrics', (req, res) => {
-  res.json(data);
+app.get('/metrics', async (req, res) => {
+  if ( data ){
+    res.send(data.replace(/\n/g, '<br>'));
+  } else{
+    res.send('No data exist');
+  }
 });
 
+app.get('/healthcheck', (req, res) => {
+  res.status(200).send('OK');
+});
+
+
 const server = app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-  
-module.exports = { app, server, fetchDataAndUpdate }; 
+  console.log(`Server is listening on port ${port}`);
+});
+
+module.exports = {
+  app,
+  server,
+  fetchDataAndUpdate
+};
